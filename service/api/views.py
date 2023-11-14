0f2adb67181c09fe9a880608d949548c342d67a8
Alexander Lakiza <285469@edu.itmo.ru>
2023-11-14 17:37:31 +0300

    Модель заработала | В лидерборде появился | Тесты добавил

--- service/api/views.py
+++ service/api/views.py
@@ -1,7 +1,9 @@
 from typing import List
 
-from fastapi import APIRouter, FastAPI, Request, HTTPException
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, Request
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
+from starlette import status
 
 from service.api.exceptions import UserNotFoundError
 from service.log import app_logger
@@ -14,12 +16,25 @@ class RecoResponse(BaseModel):
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    # You can add your own logic to validate the token here
+    if token != "EHASMJLYDWHJKESU":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return token
+
 
 @router.get(
     path="/health",
     tags=["Health"],
 )
-async def health() -> str:
+async def health(token: str = Depends(get_current_user)) -> str:
     return "I am alive"
 
 
@@ -29,13 +44,12 @@ async def health() -> str:
     response_model=RecoResponse,
 )
 async def get_reco(
-    request: Request,
-    model_name: str,
-    user_id: int,
+    request: Request, model_name: str, user_id: int,
+    token: str = Depends(get_current_user)
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
-    if user_id > 10**9:
+    if user_id > 10 ** 9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
     k_recs = request.app.state.k_recs
@@ -45,18 +59,15 @@ async def get_reco(
     if model_name == "initial_recsys_model":
         reco = [_ * 3 for _ in range(k_recs)]
     else:
-        raise HTTPException(status_code=404,
-                            detail="Incorrect model name")
+        raise HTTPException(status_code=404, detail="Incorrect model name")
 
     return RecoResponse(user_id=user_id, items=reco)
 
 
-@router.get(
-    path="/hello",
-    tags=["test"]
-)
+@router.get(path="/hello", tags=["test"])
 async def hello_world() -> str:
     return "Hello, world!"
+
 
 def add_views(app: FastAPI) -> None:
     app.include_router(router)
--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -10,8 +10,14 @@ GET_RECO_PATH = "/reco/{model_name}/{user_id}"
 def test_health(
     client: TestClient,
 ) -> None:
+    """
+    Проверка доступа к /health
+    """
+    valid_token = "EHASMJLYDWHJKESU"
+
     with client:
-        response = client.get("/health")
+        response = client.get("/health", headers={
+            "Authorization": f"Bearer {valid_token}"})
     assert response.status_code == HTTPStatus.OK
 
 
@@ -19,10 +25,16 @@ def test_get_reco_success(
     client: TestClient,
     service_config: ServiceConfig,
 ) -> None:
+    """
+    Проверка работы эндпоинта для модели
+    """
     user_id = 123
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    valid_token = "EHASMJLYDWHJKESU"
+    path = GET_RECO_PATH.format(model_name="initial_recsys_model",
+                                user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={
+            "Authorization": f"Bearer {valid_token}"})
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -33,9 +45,75 @@ def test_get_reco_success(
 def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
-    user_id = 10**10
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    """
+    Проверка наличия ошибки 404 при неправильном id пользователя
+    """
+    valid_token = "EHASMJLYDWHJKESU"
+    user_id = 10 ** 10
+    path = GET_RECO_PATH.format(model_name="initial_recsys_model",
+                                user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={
+            "Authorization": f"Bearer {valid_token}"})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
+
+
+def test_not_found_error(
+    client: TestClient,
+) -> None:
+    """
+    Предусмотреть, чтобы сервис возвращал 404 ошибку, если задано
+    неверное имя модели (1 балл) + покрыть тестами (1 балл)
+    """
+    valid_token = "EHASMJLYDWHJKESU"
+    incorrect_path = GET_RECO_PATH.format(model_name="bla_bla_model",
+                                          user_id=321)
+
+    correct_path = GET_RECO_PATH.format(model_name="initial_recsys_model",
+                                        user_id=321)
+
+    with client:
+        bad_response = client.get(incorrect_path, headers={
+            "Authorization": f"Bearer {valid_token}"})
+        good_response = client.get(correct_path, headers={
+            "Authorization": f"Bearer {valid_token}"})
+
+    assert bad_response.status_code == HTTPStatus.NOT_FOUND
+    assert good_response.status_code == HTTPStatus.OK
+
+
+def test_authorization_with_any_token(
+    client: TestClient,
+) -> None:
+    """
+    Добавить аутентификацию (1 балл) + тесты на нее (1 балл)
+    (Проверка правильного/неправильного токена)
+    """
+    valid_token = "EHASMJLYDWHJKESU"
+    invalid_token = "BLABLATOKEN"
+
+    with client:
+        good_response = client.get("/reco/initial_recsys_model/432", headers={
+            "Authorization": f"Bearer {valid_token}"})
+
+        bad_response = client.get(
+            "/reco/initial_recsys_model/432",
+            headers={"Authorization": f"Bearer {invalid_token}"}
+        )
+
+    assert good_response.status_code == HTTPStatus.OK
+    assert bad_response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+def test_authorization_with_empty_token(
+    client: TestClient,
+) -> None:
+    """
+    Добавить аутентификацию (1 балл) + тесты на нее (1 балл)
+    (Проверка отсутствующего токена)
+    """
+    with client:
+        response = client.get("/reco/initial_recsys_model/432")
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
